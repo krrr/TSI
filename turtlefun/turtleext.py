c5c8b12c84b48e7d74a8da627a16fc4b71292e79
krrr <guogaishiwo@gmail.com>
2015-04-03 17:50:24 +0800

    Update turtle ext

--- turtlefun/turtleext.py
+++ turtlefun/turtleext.py
@@ -8,41 +8,33 @@ tsi_ext_flag = None
 
 
 def setup(env):
-    turtle.title("Scheme Turtle Graphics")
-    turtle.mode('logo')
-
     # add procedures to env
     pairs = [(name, SPrimitiveProc(imp, name)) for (name, imp) in _procedures]
     env.extend(pairs)
 
 
-def _get_int(obj):
-    if not isinstance(obj, SNumber): raise ValueError('number expected')
-    return obj.num
-
-
 def forward(n):
     """Move the turtle forward a distance N units on the current heading."""
-    turtle.forward(_get_int(n))
+    turtle.forward(n)
     return theNil
 
 
 def backward(n):
     """Move the turtle backward a distance N units on the current heading,
     without changing direction."""
-    turtle.backward(_get_int(n))
+    turtle.backward(n)
     return theNil
 
 
 def left(n):
     """Rotate the turtle's heading N degrees counterclockwise."""
-    turtle.left(_get_int(n))
+    turtle.left(n)
     return theNil
 
 
 def right(n):
     """Rotate the turtle's heading N degrees clockwise."""
-    turtle.right(_get_int(n))
+    turtle.right(n)
     return theNil
 
 
@@ -52,7 +44,7 @@ def circle(r, extent=None):
     of the circle only. Draws in the clockwise direction if R is negative,
     and otherwise counterclockwise, leaving the turtle facing along the
     arc at its end."""
-    turtle.circle(_get_int(r), _get_int(extent) if extent else None)
+    turtle.circle(r, extent)
     return theNil
 
 
@@ -68,13 +60,13 @@ def vec2d_abs(v):
 
 def setposition(x, y):
     """Set turtle's position to (X,Y), heading unchanged."""
-    turtle.setposition(_get_int(x), _get_int(y))
+    turtle.setposition(x, y)
     return theNil
 
 
 def setheading(h):
     """Set the turtle's heading H degrees clockwise from north (up)."""
-    turtle.setheading(_get_int(h))
+    turtle.setheading(h)
     return theNil
 
 
@@ -97,7 +89,7 @@ def showturtle():
 
 
 def hideturtle():
-    """Make turtle visible."""
+    """Make turtle invisible."""
     turtle.hideturtle()
     return theNil
 
@@ -141,12 +133,22 @@ def speed(s):
     """Set the turtle's animation speed as indicated by S (an integer in
     0-10, with 0 indicating no animation (lines draw instantly), and 1-10
     indicating faster and faster movement."""
-    turtle.speed(_get_int(s))
+    turtle.speed(s)
     return theNil
 
 
+def tracer(n=None, delay=None):
+    """If n is given, only each n-th regular screen update is really performed.
+    Also set delay if given."""
+    if n is None and delay is None:
+        return SNumber(turtle.tracer())
+    else:
+        turtle.tracer(n, delay)
+        return theNil
+
+
 _procedures = (
-    ('forward', forward),
+    ('forward', forward), ('fd', forward),
     ('backward', backward), ('back', backward),
     ('left', left),
     ('right', right),
@@ -154,8 +156,8 @@ _procedures = (
     ('setposition', setposition), ('setpos', setposition),
     ('position', position), ('pos', position),
     ('setheading', setheading),
-    ('penup', penup),
-    ('pendown', pendown),
+    ('penup', penup), ('pu', penup),
+    ('pendown', pendown), ('pd', pendown),
     ('showturtle', showturtle),
     ('hideturtle', hideturtle),
     ('clear', clear),
@@ -165,4 +167,5 @@ _procedures = (
     ('vec2d_abs', vec2d_abs),
     ('exitonclick', exitonclick),
     ('speed', speed),
+    ('tracer', tracer),
 )
