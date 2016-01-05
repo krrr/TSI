102458235b822dedf278b2672eff3b558268d8f1
krrr <guogaishiwo@gmail.com>
2016-01-05 04:43:57 +0000

    Update call/cc test

--- tests/call-cc-generator.scm
+++ tests/call-cc-generator.scm
@@ -1,31 +1,29 @@
-(define (println a) (display a) (newline))
+; this example comes from wikipedia page of call/cc
 
-; this example is from wikipedia page of call/cc
-
-; make a generator that generate eveny element of lst un touched
+; make a generator that generate every element of lst untouched
 (define (gen lst)
   (define (control-state return)
-    (for-each 
-     (lambda (element)
-       ; The next call to (call/cc control-state) will resume
-       ; with a newly obtained continuation (outside the gen proc),
-       ; so we set return to it. For Racket it's unnecessary, which
-       ; is unusual.
-       (set! return
-         (call/cc (lambda (cont)
-           (set! control-state cont)
-           (return element)))))
-     lst)
+    (for-each (lambda (i)
+      ; The next call to (call/cc control-state) will resume
+      ; with a newly obtained continuation (outside the gen proc),
+      ; so we set return to it. For Racket it's unnecessary, which
+      ; is unusual.
+      (set! return
+        (call/cc (lambda (cont)
+          (set! control-state cont)
+          (return i)))))
+    lst)
     (return 'you-fell-off-the-end))
  
-  ; make a closure that holds all status
+  ; make a closure that holds the control-state
   (lambda () (call/cc control-state)))
 
 
 ;(define g (gen '(1 2 3)))
-;(println "generator created")
-;(println (g))
-;(println (g))
-;(println (g))
-;(println (g))
-;(println (g))
+
+;(print "generator created")
+;(print (g))
+;(print (g))
+;(print (g))
+;(print (g))
+;(print (g))
--- tsi/__init__.py
+++ tsi/__init__.py
@@ -11,12 +11,12 @@ from .core import SchemeError
 
 def main_entry():
     sys.path.append(os.getcwd())  # for finding extension module
-    e = Evaluator()
+    eva = Evaluator()
     if len(sys.argv) > 1:
         try:
-            e.load_file(sys.argv[1])
-        except SchemeError:
+            eva.load_file(sys.argv[1])
+        except (SchemeError, FileNotFoundError) as e:
             print('Error: %s' % e, file=sys.stderr)
             sys.exit(-1)
     else:
-        e.driver_loop()
+        eva.driver_loop()
